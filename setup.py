bcd23c7f1963f6dbe102ec9614f504ac93d8c331
Asser Schrøder Femø <asser@diku.dk>
2012-12-07 11:27:04 +0100

    Bump version to 0.3.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='googleanalytics',
-    version='0.3.2',
+    version='0.3.3',
     author='Asser Schrøder Femø',
     author_email='asser@diku.dk',
     packages=['googleanalytics','googleanalytics.test','googleanalytics.request'],
