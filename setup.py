87db89cbf255f2520e0febc63f6223304f1182d4
Asser Schrøder Femø <asser@diku.dk>
2013-06-25 11:24:21 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,14 +5,13 @@ from distutils.core import setup
 
 setup(
     name='googleanalytics',
-    version='0.3.7',
+    version='0.3.8',
     author='Asser Schrøder Femø',
     author_email='asser@diku.dk',
-    packages=['googleanalytics','googleanalytics.test','googleanalytics.request'],
+    packages=['googleanalytics', 'googleanalytics.test', 'googleanalytics.request'],
     scripts=[],
     url='http://github.com/asser/python-ga',
     license='LICENSE.txt',
     description='Google Analytics client',
     long_description=open('README.md').read(),
 )
-
